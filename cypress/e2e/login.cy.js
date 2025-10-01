71104175884be0a144237a7c4f899c407f23a5d0
Thais Moreira <thais.dfmoreira@gmail.com>
2025-10-01 10:26:09 -0300

    Adicionando beforeEach para o acesso a página de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,11 @@
 describe('login', () => {
-  it('Login com dados válidos devem permitir entrada no sistema', () => {
+  beforeEach(() => {
     // Arrange
     cy.visit('http://localhost:4000')
 
+  })
+
+  it('Login com dados válidos devem permitir entrada no sistema', () => {
     // Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
@@ -13,9 +16,7 @@ describe('login', () => {
   })
 
   it('Login com dados inválidos devem apresentar mensagem de erro', () => {
-    // Arrange
-    cy.visit('http://localhost:4000')
-
+    
     // Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('654321')
