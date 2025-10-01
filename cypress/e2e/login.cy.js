831ebbd18022244163225543c3efb544c1790292
Thais Moreira <thais.dfmoreira@gmail.com>
2025-10-01 14:45:19 -0300

    Adicionando screenshots

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,14 +2,17 @@ describe('login', () => {
   beforeEach(() => {
     // Arrange
     cy.visit('http://localhost:4000')
+    cy.screenshot('após-visitar-página')
 
   })
 
-  it('Login com dados válidos devem permitir entrada no sistema', () => {
+  it.only('Login com dados válidos devem permitir entrada no sistema', () => {
     // Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
+    cy.screenshot('após-preencher-dados-válidos')
     cy.contains('button', 'Entrar').click()
+    cy.screenshot('após-clicar-no-botão-entrar')
 
     // Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
