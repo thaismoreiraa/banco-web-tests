0454dc5a3e8c428eb60014b1fa3ab0c97cddc17a
Thais Moreira <thais.dfmoreira@gmail.com>
2025-10-01 09:57:34 -0300

    Adicionando teste para validar login com credenciais inválidas

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,9 +6,22 @@ describe('login', () => {
     // Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.get('#login-section > .btn').click()
+    cy.contains('button', 'Entrar').click()
 
     // Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
+  })
+
+  it('Login com dados inválidos devem apresentar mensagem de erro', () => {
+    // Arrange
+    cy.visit('http://localhost:4000')
+
+    // Act
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('654321')
+    cy.contains('button', 'Entrar').click()
+
+    // Assert
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
   })
 })
