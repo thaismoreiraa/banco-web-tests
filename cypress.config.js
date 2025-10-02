df3ba249ce81da13ce07a2f0c1d6295fde3bc698
Thais Moreira <thais.dfmoreira@gmail.com>
2025-10-02 15:13:28 -0300

    Adicionando o relatório HTML do Mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4000',
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
-});
+})
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,6 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+
+// Relatório de testes em HTML:
+import 'cypress-mochawesome-reporter/register'
